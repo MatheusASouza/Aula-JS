d0aacc62e9d354e1961425476f341894272e7010
Matheus Alves de Souza <79257255+MatheusASouza@users.noreply.github.com>
2021-05-31 10:07:59 -0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,4 +1,9 @@
-
+// Variáveis:
 var nome = "Matheus Alves";
-var idade = "20";
+var idade = 20;
+
+// Janela de alerta com concatenação entre string e variáveis utilizando o "+"
 alert(nome + " tem " + idade + " anos");
+
+// Janela de prompt (para o usuário escrever uma mensagem)
+prompt(Digite seu nome: );
